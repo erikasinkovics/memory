abc1df421da2e2700af81f314b11f683e6fc8527
erikasinkovics <eperjesi.erika@gmail.com>
2020-02-06 15:33:43 -0500

    Creating static components, adding basic CSS.

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+    return (
+        <p>Footer</p>
+    )
+
+};
+
+
+export default Footer;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import './Header.css';
 
 function Header() {
-
+    return (
+        <div className='Header'>
+            <h1>MEMORY</h1>
+        </div>
+    )
 
 };
 
