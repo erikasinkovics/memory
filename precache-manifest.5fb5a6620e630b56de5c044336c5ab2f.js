150ac22195b34d441f3d41c254a6e6b4446d094c
Erika Sinkovics <erika.sinkovics@outlook.com>
2020-09-08 16:06:26 -0400

    Updates

---
+++ precache-manifest.5fb5a6620e630b56de5c044336c5ab2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "099eb60b659136983606abe6b545f7ec",
+    "url": "/memory/index.html"
+  },
+  {
+    "revision": "9aeaf34b26323e701539",
+    "url": "/memory/static/css/main.533d39e2.chunk.css"
+  },
+  {
+    "revision": "ec016de2eaaf04a93db8",
+    "url": "/memory/static/js/2.2dad4d57.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/memory/static/js/2.2dad4d57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9aeaf34b26323e701539",
+    "url": "/memory/static/js/main.afea25a0.chunk.js"
+  },
+  {
+    "revision": "62e0eae13c9afaa66ed5",
+    "url": "/memory/static/js/runtime-main.c4984c0b.js"
+  }
+]);
